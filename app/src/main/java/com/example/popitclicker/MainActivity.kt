a24ee1c14aa75b3196d6efe510f8ef3b47d99375
7sug <baikal728@gmail.com>
2021-10-14 16:35:25 +0300

    Добавлено обновление данных после захода в приложение, добавлена кнопка "Обновить", которая принудительно обновляет данные о погоде

--- app/src/main/java/com/example/popitclicker/MainActivity.kt
+++ app/src/main/java/com/example/popitclicker/MainActivity.kt
@@ -4,48 +4,57 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import org.jetbrains.anko.doAsync
 import org.json.JSONObject
-import java.net.URI
 import java.net.URL
+import kotlinx.coroutines.*
+import org.jetbrains.anko.uiThread
+
 
 class MainActivity : AppCompatActivity() {
-    private var user_city: EditText? = null
     private var city_button: Button? = null
     private var result_info: TextView? = null
     @SuppressLint("SetTextI18n")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        user_city = findViewById(R.id.user_city)
         city_button = findViewById(R.id.weather_button)
         result_info = findViewById(R.id.result)
-
-        city_button?.setOnClickListener{
-            if(user_city?.text?.toString()?.trim()?.equals("")!!)
-                Toast.makeText(this, "Введите город!", Toast.LENGTH_LONG).show()
-            else
-            {
-                val city: String = user_city?.text.toString()
-                val key: String = "56756dbbccacde32db6015b664c3df49"
-                val url: String = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$key&units=metric&lang=ru"
-
-                doAsync {
-                    val apiResponse = URL(url).readText()
-                    val weather = JSONObject(apiResponse).getJSONArray("weather")
-                    val desc = weather.getJSONObject(0).getString("description")
-                    val main = JSONObject(apiResponse).getJSONObject("main")
-                    val temp = main.getString("temp")
-                    result_info?.text = "Температура: $temp\n$desc"
-                }
+    }
 
 
+    @SuppressLint("SetTextI18n")
+    private fun getWeather() {
+        val city = "Пенза"
+        val key = "56756dbbccacde32db6015b664c3df49"
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$key&units=metric&lang=ru"
+        doAsync {
+            val apiResponse = URL(url).readText()
+            val weather = JSONObject(apiResponse).getJSONArray("weather")
+            val desc = weather.getJSONObject(0).getString("description")
+            val main = JSONObject(apiResponse).getJSONObject("main")
+            val temp = main.getString("temp") + "°С"
+            uiThread {
+                result_info?.text = "$temp\n$desc"
+            }
             }
         }
 
+    override fun onStart() {
+        super.onStart()
+        getWeather()
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        city_button?.setOnClickListener{
+            getWeather()
+            Toast.makeText(this, "Данные обновлены!", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
